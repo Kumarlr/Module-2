717e4396a99715a75d0301f33dd6b98607c74a9f
Kumarlr <157813636+Kumarlr@users.noreply.github.com>
2024-02-07 22:43:35 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,41 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    string public name;
+    string public email;
+    string public phone;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event ProfileUpdated(string name, string email, string phone);
 
-    constructor(uint initBalance) payable {
+    constructor(uint initBalance, string memory _name, string memory _email, string memory _phone) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+        name = _name;
+        email = _email;
+        phone = _phone;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
 
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -48,13 +47,30 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function userProfile() public view returns (string memory, string memory, string memory) {
+        string memory defaultName = "Chethan";
+        string memory defaultEmail = "chethan@example.com";
+        string memory defaultPhone = "1234567890";
+        
+        // Return default details if any of the profile fields are empty
+        if (bytes(name).length == 0 || bytes(email).length == 0 || bytes(phone).length == 0) {
+            return (defaultName, defaultEmail, defaultPhone);
+        }
+        
+        return (name, email, phone);
+    }
+
+    function updateProfile(string memory _name, string memory _email, string memory _phone) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        name = _name;
+        email = _email;
+        phone = _phone;
+        emit ProfileUpdated(name, email, phone);
     }
 }
